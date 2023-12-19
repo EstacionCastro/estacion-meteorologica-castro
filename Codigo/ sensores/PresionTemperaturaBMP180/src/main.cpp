44917060f3762e5ccc12226553e5d86b914b1730
Cristopher Santaiti <152980135+KyriosPulce@users.noreply.github.com>
2023-12-19 13:10:10 +0100

    Update main.cpp

--- Codigo/ sensores/PresionTemperaturaBMP180/src/main.cpp
+++ Codigo/ sensores/PresionTemperaturaBMP180/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+SFE_BMP180 bmp180; //creamos el objeto de tipo SFE_BMP180
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+
+  if (bmp180.begin())
+    Serial.println("BMP180 iniciado correctamente");
+  else
+  {
+    Serial.println("Error al iniciar el BMP180");
+    while(1); // bucle infinito, algo salió mal. Comprueba las conexiones.
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  char status;
+  double T,P;
+  
+  status = bmp180.startTemperature();//Inicio de lectura de temperatura
+  if (status != 0)
+  {   
+    delay(status); //Pausa para que finalice la lectura
+    status = bmp180.getTemperature(T); //Obtener la temperatura
+    if (status != 0)
+    {
+      status = bmp180.startPressure(3);//Inicio lectura de presión
+      if (status != 0)
+      {        
+        delay(status);//Pausa para que finalice la lectura        
+        status = bmp180.getPressure(P,T);//Obtenemos la presión
+        if (status != 0)
+        {                  
+          Serial.print("Temperatura: ");
+          Serial.print(T);
+          Serial.print(" *C , ");
+          Serial.print("Presion: ");
+          Serial.print(P);
+          Serial.print(" mb , ");      
+        }      
+      }      
+    }   
+  } 
+  delay(1000);
 }
